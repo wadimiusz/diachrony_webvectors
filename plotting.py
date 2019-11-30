e935641847018ee2125c846f2e538d77265523e5
Vlad Mikhailov <43072268+vmkhlv@users.noreply.github.com>
2019-11-30 20:09:54 +0300

    added tsne semantic shift viz

--- plotting.py
+++ plotting.py
@@ -21,62 +21,46 @@ path = config.get('Files and directories', 'font')
 font = font_manager.FontProperties(fname=path)
 
 
-def tsne_semantic_shifts(array, word_labels, word_vectors):
+def tsne_semantic_shifts(result, fname):
+    """
+    :result: a dictionary containing word_list and vector_list from aligned models,
+    and model_number to plot trajectories
+    """
+    word_list = result["word_list"]
+    vector_list = result["vector_list"]
+    model_number = result["model_number"]
 
+    embed = TSNE(n_components=2, random_state=0, learning_rate=150, init="pca")
     np.set_printoptions(suppress=True)
-    embedding = TSNE(n_components=2, random_state=0, learning_rate=150, init="pca")
-    y = embedding.fit_transform(array)
-    word_coordinates = [y[i] for i in range(0, len(word_vectors) + 1)]
+    y = embed.fit_transform(np.array(vector_list))
+
+    word_coordinates = [y[i] for i in range(0, model_number)]
     x_coordinates, y_coordinates = y[:, 0], y[:, 1]
 
     plot.scatter(x_coordinates, y_coordinates)
-    plot.axis('off')
+    plot.axis("off")
 
-    for label, x, y in zip(word_labels, x_coordinates, y_coordinates):
-        plot.annotate(label, xy=(x, y), xytext=(-len(label)*4.5, 4), textcoords='offset points')
+    for label, x, y in zip(word_list, x_coordinates, y_coordinates):
+        plot.annotate(
+            label, xy=(x, y), xytext=(-len(label) * 4.5, 4), textcoords="offset points"
+        )
+
     plot.xlim(x_coordinates.min() - 10, x_coordinates.max() + 10)
     plot.ylim(y_coordinates.min() - 10, y_coordinates.max() + 10)
-    for i in range(len(word_coordinates) - 1, 0, -1):
-        plot.annotate("", xy=(word_coordinates[i - 1][0], word_coordinates[i - 1][1]),
-                     xytext=(word_coordinates[i][0], word_coordinates[i][1] + 5),
-                     arrowprops=dict(arrowstyle='-|>', color='indianred'))
 
-    plot.savefig(root + 'data/images/tsne_semantic_shift/' + modelname + '_' + fname + '.png',
-                 dpi=150, bbox_inches='tight')
+    for i in range(len(word_coordinates) - 1, 0, -1):
+        plot.annotate(
+            "",
+            xy=(word_coordinates[i - 1][0], word_coordinates[i - 1][1]),
+            xytext=(word_coordinates[i][0], word_coordinates[i][1] + 5),
+            arrowprops=dict(arrowstyle="-|>", color="indianred"),
+        )
+
+    plot.savefig(
+        root + "data/images/tsne_shift/" + fname + ".png", dpi=150, bbox_inches="tight"
+    )
     plot.close()
     plot.clf()
-
-
-def vizualize_semantic_shifts(all_similar_words, word_vectors, model, word, year, kind="TSNE"):
-
-    rows = len(word_vectors) + len(all_similar_words) + 1
-    array = np.empty((rows, model.vector_size), dtype='f')
-    word_labels = [word + ' ' + year]
-
-    num = int(year) - 1
-
-    array[0, :] = model[word]
-
-    row_counter = 1
-
-    for i in range(len(word_vectors)):
-        array[row_counter, :] = word_vectors[i]
-        row_counter += 1
-        word_labels.append(word + ' ' + str(num))
-        num -= 1
-
-    for word_ in all_similar_words:
-        word_vector = model[word_]
-        word_labels.append(word_)
-        array[row_counter, :] = word_vector
-        row_counter += 1
-
-    word_labels = [re.sub(r'_[A-Z]+', '', w) for w in word_labels]
-
-    if kind.lower() == "tsne":
-        return tsne_semantic_shifts(array, word_labels, word_vectors)
-    else:
-        raise ValueError("Kind is {}, must be TSNE".format(kind))
 
 
 def singularplot(word, modelname, vector, fname):
