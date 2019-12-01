be80ad6da0751fe6500983c953888dac47d7579e
Julia Rodina <julia.rodina97@gmail.com>
2019-12-01 16:01:46 +0300

    argparse

--- make_pickles.py
+++ make_pickles.py
@@ -1,17 +1,25 @@
 import gensim
 import pickle
 import pandas as pd
+from argparse import ArgumentParser
+from os import path
+
+parser = ArgumentParser()
+parser.add_argument('--models', '-m', required=True, help='path to the word embeddings models')
+parser.add_argument('--corpora', '-c', required=True, help='path to the corpora')
+args = parser.parse_args()
 
 corpuses = {}
 for year in range(2015, 2020):
-    df = pd.read_csv('corpora/tables/{year}_contexts.csv.gz'.format(year=year),
-                     index_col='ID')
+    df = pd.read_csv(
+        path.join(args.models, 'corpora/tables/{year}_contexts.csv.gz'.format(year=year)),
+        index_col='ID')
     corpuses.update({year: df})
 
 models = {}
 for year in range(2015, 2020):
     model = gensim.models.KeyedVectors.load_word2vec_format(
-        'models/{year}_0_5.bin'.format(year=year),
+        path.join(args.models, '{year}_0_5.bin'.format(year=year)),
         binary=True, unicode_errors='replace')
     model.init_sims(replace=True)
 
