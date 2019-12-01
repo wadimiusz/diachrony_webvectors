374b86aeed21f78137ae3724f02dffe86d275aa2
akutuzov <akutuzov72@gmail.com>
2019-12-01 15:04:59 +0100

    Better pickles creation

--- make_pickles.py
+++ make_pickles.py
@@ -1,8 +1,10 @@
+import sys
 import gensim
 import pickle
 import pandas as pd
 from argparse import ArgumentParser
 from os import path
+from smart_open import open
 
 parser = ArgumentParser()
 parser.add_argument('--models', '-m', required=True, help='path to the word embeddings models')
@@ -10,35 +12,31 @@ parser.add_argument('--corpora', '-c', required=True, help='path to the corpora'
 args = parser.parse_args()
 
 corpuses = {}
-for year in range(2015, 2020):
+for year in range(2010, 2020):
     df = pd.read_csv(
-        path.join(args.models, 'corpora/tables/{year}_contexts.csv.gz'.format(year=year)),
+        path.join(args.models, '{year}_contexts.csv.gz'.format(year=year)),
         index_col='ID')
     corpuses.update({year: df})
 
-models = {}
-for year in range(2015, 2020):
-    model = gensim.models.KeyedVectors.load_word2vec_format(
-        path.join(args.models, '{year}_0_5.bin'.format(year=year)),
-        binary=True, unicode_errors='replace')
-    model.init_sims(replace=True)
+vocabs = {}
+for year in range(2010, 2020):
+    model = gensim.models.KeyedVectors.load(
+        path.join(args.models, '{year}_rnc_incremental.model'.format(year=year)))
+    vocabs[year] = set(model.index2word)
 
-    models.update({year: model})
-
-vocabs = [model.vocab for model in list(models.values())]
-united_vocab = set.union(*map(set, vocabs))
-print(len(united_vocab))
+united_vocab = set.union(*map(set, vocabs.values()))
+print('Total words:', len(united_vocab), file=sys.stderr)
 
 for word in united_vocab:
-    dict = {}
-    for year, model in models.items():
-        if word in model.vocab:
+    out_dict = {}
+    for year in vocabs:
+        if word in vocabs[year]:
             corpus = corpuses.get(year)
             samples = []
             for idx, lemmas, raw in corpus[['LEMMAS', 'RAW']].itertuples():
                 lemmas_split = lemmas.split()
-                if word in lemmas.split():
-                    samples.append([lemmas.split(), raw])
-            dict.update({year: samples})
-    with open('pickles/{word}.pickle'.format(word=word), 'wb') as f:
-        pickle.dump(dict, f)
+                if word in lemmas_split:
+                    samples.append([lemmas_split, raw])
+            out_dict.update({year: samples})
+    with open('pickles/{word}.pickle.gz'.format(word=word), 'wb') as f:
+        pickle.dump(out_dict, f)
